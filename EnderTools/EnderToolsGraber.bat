c4e6fd33f06ccf841ed0ad3d7337e8a69372a989
EnderActually <99933312+EnderActually@users.noreply.github.com>
2023-12-12 15:23:23 -0500

    Update EnderToolsGraber.bat

--- EnderTools/EnderToolsGraber.bat
+++ EnderTools/EnderToolsGraber.bat
@@ -3,6 +3,8 @@ title Ender's Tools
 GOTO INITIALIZE
 
 :INITIALIZE
+If %LAUNCHER% == 0 pause
+If %LAUNCHER% == 0 echo invalid dir
 If %LAUNCHER% == 0 EXIT
 SET STATUS=2
 SET DLINK=https://raw.githubusercontent.com/EnderActually/EnderToolsPublic/main/EnderTools/EnderTools.bat
